20738ee4b3e801db199d68d4538dd81a2f1e7ca2
123110443 PEDRO HENRIQUE MALAQUIAS DA SILVA <pedro.henrique.malaquias.silva@lcc2-56.lcc.ufcg.edu.br>
2024-07-23 07:03:45 -0300

    Inicio do Lab

---
+++ RR4-01-Roteiro-Revisao-Recursao-environment/src/main/java/recursao/TestarMetodosRecursivos.java
@@ -0,0 +1,11 @@
+package recursao;
+
+public class TestarMetodosRecursivos {
+	public static void main(String[] args) {
+		//int[] array = {1,2,3,4,5};
+		MetodosRecursivos mr = new MetodosRecursivos();
+		//System.out.println(mr.calcularSomaArray(array, 4));
+
+		System.out.println(mr.calcularFibonacci(5));
+	}
+}
